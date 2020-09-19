b2684623b3c1fad6917a741e1f81949d9f70f206
Felipe Tomazelli <fefetl08@gmail.com>
2020-09-19 18:33:08 -0300

    feat: metodo para abrir arquivos

--- BarreiraReusavel/ManipularArquivo.java
+++ BarreiraReusavel/ManipularArquivo.java
@@ -1,13 +1,24 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class ManipularArquivo implements Serializable {
 
-    public static void salvar(String nomeDoArquivo, Object dados) throws IOException {
+    public static void salvar(String nomeDoArquivo, ArrayList<Integer> dados) throws IOException {
         FileOutputStream arquivo = new FileOutputStream(nomeDoArquivo);
         ObjectOutputStream gravador = new ObjectOutputStream(arquivo);
         gravador.writeObject(dados);
         gravador.close();
         arquivo.close();
+    }
+
+    public static ArrayList<Integer> abrir(String nomeDoArquivo) throws IOException, ClassNotFoundException {
+        ArrayList<Integer> listaDeInts = null;
+        FileInputStream arquivo = new FileInputStream(nomeDoArquivo);
+        ObjectInputStream restaurador = new ObjectInputStream(arquivo);
+        listaDeInts = (ArrayList<Integer>) restaurador.readObject();
+        restaurador.close();
+        arquivo.close();
+        return listaDeInts;
     }
 
 }
