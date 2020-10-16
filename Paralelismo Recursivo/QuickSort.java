617ff3328f5bce70a880529c6ef5ec6bdc0463f7
Felipe Tomazelli <fefetl08@gmail.com>
2020-10-16 12:56:23 -0300

    feat: quicksort paralelo, sem controle de threads

--- Paralelismo Recursivo/QuickSort.java
+++ Paralelismo Recursivo/QuickSort.java
@@ -1,49 +1,72 @@
 // Java program for implementation of QuickSort 
-class QuickSort { 
-    /* This function takes last element as pivot, 
-       places the pivot element at its correct 
-       position in sorted array, and places all 
-       smaller (smaller than pivot) to left of 
-       pivot and all greater elements to right 
-       of pivot */
-    int partition(int arr[], int low, int high) { 
+class QuickSort extends Thread {
+
+    int low, high;
+    int[] arr;
+
+    public QuickSort(int[] arr, int low, int high) {
+        this.arr = arr;
+        this.low = low;
+        this.high = high;
+    }
+
+    int partition(int[] arr, int low, int high) { 
         int pivot = arr[high];  
-        int i = (low-1); // index of smaller element 
-        for (int j=low; j<high; j++) { 
-            // If current element is smaller than the pivot 
+        int i = (low - 1); 
+        for (int j = low; j < high; j++) { 
+            
             if (arr[j] < pivot) { 
                 i++; 
   
-                // swap arr[i] and arr[j] 
                 int temp = arr[i]; 
                 arr[i] = arr[j]; 
                 arr[j] = temp; 
             } 
         } 
   
-        // swap arr[i+1] and arr[high] (or pivot) 
-        int temp = arr[i+1]; 
-        arr[i+1] = arr[high]; 
+        int temp = arr[i + 1]; 
+        arr[i + 1] = arr[high]; 
         arr[high] = temp; 
   
-        return i+1; 
+        return i + 1; 
     } 
   
-  
-    /* The main function that implements QuickSort() 
-      arr[] --> Array to be sorted, 
-      low  --> Starting index, 
-      high  --> Ending index */
-    void sort(int arr[], int low, int high) { 
+    void sort(int arr[], int low, int high) throws InterruptedException {
         if (low < high) { 
-            /* pi is partitioning index, arr[pi] is  
-              now at right place */
             int pi = partition(arr, low, high); 
   
-            // Recursively sort elements before 
-            // partition and after partition 
-            sort(arr, low, pi-1); 
-            sort(arr, pi+1, high); 
+            QuickSort left = new QuickSort(arr, low, pi - 1); 
+            QuickSort right = new QuickSort(arr, pi + 1, high);
+            
+            left.start();
+            right.start();
+
+            left.join();
+            right.join();
+            
         } 
+    }
+
+    public void run() {
+        try {
+            sort(arr, low, high);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int[] arr = { 54, 64, 95, 82, 12, 32, 63 };
+        int n = arr.length;
+
+        QuickSort qs = new QuickSort(arr, 0, n - 1);
+        qs.start();
+        qs.join();
+
+        // Print shorted elements 
+        for (int i = 0; i < n; i++) 
+            System.out.print(arr[i] + " ");
     }
 } 
