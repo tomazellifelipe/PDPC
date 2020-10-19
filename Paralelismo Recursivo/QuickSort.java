d11bf641c7254e28d3a03ee29ff2dca6fe44f3ca
Felipe Tomazelli <fefetl08@gmail.com>
2020-10-19 10:46:30 -0300

    feat: sequencial and multiThread method

--- Paralelismo Recursivo/QuickSort.java
+++ Paralelismo Recursivo/QuickSort.java
@@ -1,72 +1,85 @@
+import java.util.concurrent.Semaphore;
+
 // Java program for implementation of QuickSort 
 class QuickSort extends Thread {
 
+    final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
+    static int runningThreads = 0;
+
     int low, high;
     int[] arr;
+    Semaphore mutex;
 
-    public QuickSort(int[] arr, int low, int high) {
+    public QuickSort(int[] arr, int low, int high, Semaphore mutex ) {
         this.arr = arr;
         this.low = low;
         this.high = high;
+        this.mutex = mutex;
     }
 
-    int partition(int[] arr, int low, int high) { 
-        int pivot = arr[high];  
-        int i = (low - 1); 
-        for (int j = low; j < high; j++) { 
-            
-            if (arr[j] < pivot) { 
-                i++; 
-  
-                int temp = arr[i]; 
-                arr[i] = arr[j]; 
-                arr[j] = temp; 
-            } 
-        } 
-  
-        int temp = arr[i + 1]; 
-        arr[i + 1] = arr[high]; 
-        arr[high] = temp; 
-  
-        return i + 1; 
-    } 
-  
-    void sort(int arr[], int low, int high) throws InterruptedException {
-        if (low < high) { 
-            int pi = partition(arr, low, high); 
-  
-            QuickSort left = new QuickSort(arr, low, pi - 1); 
-            QuickSort right = new QuickSort(arr, pi + 1, high);
-            
-            left.start();
-            right.start();
+    public int partition(int[] arr, int low, int high) throws InterruptedException {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
 
-            left.join();
-            right.join();
-            
-        } 
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                mutex.acquire();
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                mutex.release();
+            }
+        }
+
+        int temp = arr[i + 1];
+        mutex.acquire();
+            arr[i + 1] = arr[high];
+            arr[high] = temp;
+        mutex.release();
+        
+        return i + 1;
+    }
+
+    public void sort(int[] arr, int low, int high) throws InterruptedException {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+    public void sortMultiThread(int[] arr, int low, int high) throws InterruptedException {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            if (runningThreads <= AVAILABLE_PROCESSORS) {
+                QuickSort left = new QuickSort(arr, low, pi - 1, mutex);
+                runningThreads++;
+
+                QuickSort right = new QuickSort(arr, pi + 1, high, mutex);
+                runningThreads++;
+
+                left.start();
+                right.start();
+
+                left.join();
+                right.join();
+            } else {
+                sort(arr, low, pi - 1);
+                sort(arr, pi + 1, high);
+            }
+
+        }
     }
 
     public void run() {
         try {
-            sort(arr, low, high);
-            
-        } catch (Exception e) {
+            sortMultiThread(arr, low, high);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-
-        int[] arr = { 54, 64, 95, 82, 12, 32, 63 };
-        int n = arr.length;
-
-        QuickSort qs = new QuickSort(arr, 0, n - 1);
-        qs.start();
-        qs.join();
-
-        // Print shorted elements 
-        for (int i = 0; i < n; i++) 
-            System.out.print(arr[i] + " ");
     }
 } 
